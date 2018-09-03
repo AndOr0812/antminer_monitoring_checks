41314e6c8d3f90fab9c3abc7d3fe7367fc8da1fd
nuii0 <nuii0@tuta.io>
2018-09-03 11:51:29 +0200

    added range parameter for check_fan

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FIXTURE_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/fixture/bin"
+FIXTURE_DIR="$( cd "$(dirname "${BASH_bash[0]}")" ; pwd -P )/fixture/bin"
 export PATH="$FIXTURE_DIR:$PATH"
 
 expect_value () {
@@ -54,66 +54,81 @@ cd ..
 echo "check_fan tests.."
 FAN_RPM=2880
 
-OVER_FAN=$((FAN_RPM++))
-UNDER_FAN=$((FAN_RPM--))
-
-OK_RANGE=$((FAN_RPM--)):$((FAN_RPM++))
-UNDER_RANGE=$((FAN_RPM-2)):$((FAN_RPM-1))
-OVER_RANGE=$((FAN_RPM+1)):$((FAN_RPM+2))
-
-echo $OVER_FAN
+OVER_FAN=$((FAN_RPM+1))
+UNDER_FAN=$((FAN_RPM-1))
 echo $UNDER_FAN
 
-echo $OK_RANGE
-echo $UNDER_RANGE
-echo $OVER_RANGE
+OK_RANGE=$((FAN_RPM-1)):$((FAN_RPM+1))
+UNDER_RANGE=$((FAN_RPM-2)):$((FAN_RPM-1))
+OVER_RANGE=$((FAN_RPM+1)):$((FAN_RPM+2))
+ILLEGAL_RANGE="20:10"
 
 ## arguments
 # no fan argument
-OUTPUT=$(source check_fan -H localhost -w 20 -c 10 2>&1)
+OUTPUT=$(bash check_fan -H localhost -w 20 -c 10 2>&1)
 expect_error "$OUTPUT" "Error if no fan given"
 
 # no hostname argument
-OUTPUT=$(source check_fan -f 1 -w 20 -c 10 2>&1)
+OUTPUT=$(bash check_fan -f 1 -w 20 -c 10 2>&1)
 expect_error "$OUTPUT" "Error if no hostname given"
 
 ##fan speed
-# ok if rpm over single warning threshold
-OUTPUT=$(source check_fan -H localhost -f 1 -w $OVER_FAN  2>&1)
+# ok if rpm over warning threshold
+OUTPUT=$(bash check_fan -H localhost -f 1 -w $UNDER_FAN  2>&1)
 expect_nagios_ok "$OUTPUT" "OK if rpm over threshold"
 
-# warning threshold undershot
-OUTPUT=$(source check_fan -H localhost -f 1 -w $UNDER_FAN 2>&1)
-expect_nagios_warning "$OUTPUT" "Warning if warning lower than fan rpm"
+# ok if rpm over critical threshold
+OUTPUT=$(bash check_fan -H localhost -f 1 -c $UNDER_FAN  2>&1)
+expect_nagios_ok "$OUTPUT" "OK if rpm over threshold"
 
-# critical threshold undershot
-output=$(source check_fan -h localhost -f 1 -c $UNDER_FAN 2>&1)
-expect_nagios_critical "$OUTPUT" "Critical if critical lower than fan rpm"
+# ok if rpm is exactly like threshold
+OUTPUT=$(bash check_fan -H localhost -f 1 -c $FAN_RPM -w $FAN_RPM  2>&1)
+expect_nagios_ok "$OUTPUT" "OK if rpm is exactly like threshold"
 
-# critical threshold undershot and warning undershot
-output=$(source check_fan -h localhost -f 1 -c $UNDER_FAN -w $UNDER_FAN 2>&1)
-expect_nagios_critical "$OUTPUT" "Critical if critical lower than fan rpm and warning lower than fan rpm"
+# under warning threshold
+OUTPUT=$(bash check_fan -H localhost -f 1 -w $OVER_FAN 2>&1)
+expect_nagios_warning "$OUTPUT" "Warning if rpm under warning"
+
+# under critical threshold
+OUTPUT=$(bash check_fan -H localhost -f 1 -c $OVER_FAN 2>&1)
+expect_nagios_critical "$OUTPUT" "Critical if rpm under critical"
+
+# under critical and warning threshold
+OUTPUT=$(bash check_fan -H localhost -f 1 -c $OVER_FAN -w $OVER_FAN 2>&1)
+expect_nagios_critical "$OUTPUT" "Critical if rpm under warning and critical"
 
 # within warning range
-OUTPUT=$(source check_fan -H localhost -f 1 -w $RANGE_OK 2>&1)
-expect_nagios_ok "$OUTPUT" "Ok if rpm insie warning range"
+OUTPUT=$(bash check_fan -H localhost -f 1 -w $OK_RANGE 2>&1)
+expect_nagios_ok "$OUTPUT" "Ok if rpm inside warning range"
 
 # within critical range
-OUTPUT=$(source check_fan -H localhost -f 1 -c $RANGE_OK 2>&1)
+OUTPUT=$(bash check_fan -H localhost -f 1 -c $OK_RANGE 2>&1)
 expect_nagios_ok "$OUTPUT" "Ok if rpm inside critical range"
 
+# illegal range
+OUTPUT=$(bash check_fan -H localhost -f 1 -c $ILLEGAL_RANGE 2>&1)
+expect_error "$OUTPUT" "Error on illegal range"
+
+# illegal range2
+OUTPUT=$(bash check_fan -H localhost -f 1 -c "gibberish" 2>&1)
+expect_error "$OUTPUT" "Error on another illegal range"
+
+# exactly like range
+OUTPUT=$(bash check_fan -H localhost -f 1 -c $FAN_RPM:$FAN_RPM 2>&1)
+expect_nagios_ok "$OUTPUT" "Ok if rpm exactly like critical range"
+
 # within critical range and within warning range
-OUTPUT=$(source check_fan -H localhost -f 1 -w $RANGE_OK -c $RANGE_OK 2>&1)
+OUTPUT=$(bash check_fan -H localhost -f 1 -w $OK_RANGE -c $OK_RANGE 2>&1)
 expect_nagios_ok "$OUTPUT" "Ok if rpm inside critical range"
 
 # under warning range
-OUTPUT=$(source check_fan -H localhost -f 1 -w $RANGE_UNDER 2>&1)
+OUTPUT=$(bash check_fan -H localhost -f 1 -w $UNDER_RANGE 2>&1)
 expect_nagios_warning "$OUTPUT" "Warning if rpm under warning range"
 
 # over critical range
-OUTPUT=$(source check_fan -H localhost -f 1 -w $RANGE_OVER 2>&1)
+OUTPUT=$(bash check_fan -H localhost -f 1 -c $OVER_RANGE 2>&1)
 expect_nagios_critical "$OUTPUT" "Critical if rpm over critical range"
 
-# outside warning and critical eange
-OUTPUT=$(source check_fan -H localhost -f 1 -w $RANGE_OVER -c $RANGE_UNDER 2>&1)
+# outside warning and critical range
+OUTPUT=$(bash check_fan -H localhost -f 1 -w $OVER_RANGE -c $UNDER_RANGE 2>&1)
 expect_nagios_critical "$OUTPUT" "Critical if rpm outside critical range and warning range"
